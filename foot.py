5d17649b932c53eb99ef844d932674279056a53b
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-06-14 10:24:22 +0530

    add both foot and weight it accordingly

--- foot.py
+++ foot.py
@@ -1,8 +1,14 @@
-from enum import auto
+import random
+from enum import Enum, auto
 
-from enums import BaseEnum
 
-
-class Foot(BaseEnum):
+class Foot(Enum):
     Left = auto()
     Right = auto()
+    Both = auto()
+
+    @classmethod
+    def random(cls):
+        return random.choices(
+            [cls.Left, cls.Right, cls.Both], weights=[18, 80, 2], k=1
+        )[0]
