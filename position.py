04d81c71f208a8c5c9b602134635e4245fe25a65
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-06-14 10:32:00 +0530

    add role weights to random role picking

--- position.py
+++ position.py
@@ -12,6 +12,14 @@ class Role(BaseEnum):
     Keeper = auto()
 
 
+_ROLE_WEIGHTS = {
+    Role.Attacker: 0.30,
+    Role.Midfielder: 0.30,
+    Role.Defender: 0.30,
+    Role.Keeper: 0.10,
+}
+
+
 class Position(BaseEnum):
     ST = auto()
     LW = auto()
@@ -31,53 +39,64 @@ class Position(BaseEnum):
 
     @property
     def role(self) -> Role:
-        _MAP = {
-            Position.LW: Role.Attacker,
-            Position.ST: Role.Attacker,
-            Position.RW: Role.Attacker,
-            # ----------------------------
-            Position.LM: Role.Midfielder,
-            Position.CM: Role.Midfielder,
-            Position.CAM: Role.Midfielder,
-            Position.CDM: Role.Midfielder,
-            Position.RM: Role.Midfielder,
-            # ----------------------------
-            Position.LB: Role.Defender,
-            Position.CB: Role.Defender,
-            Position.RB: Role.Defender,
-            # ----------------------------
-            Position.GK: Role.Keeper,
-        }
-        return _MAP[self]
+        return _ROLE_MAP[self]
 
     @property
     def defensiveness(self) -> float:
-        _MAP = {
-            Position.LB: 2.0,
-            Position.CB: 2.0,
-            Position.RB: 2.0,
-            Position.LM: 1.0,
-            Position.CDM: 1.5,
-            Position.CM: 1.0,
-            Position.CAM: 0.5,
-            Position.RM: 1.0,
-            Position.LW: 0.0,
-            Position.ST: 0.0,
-            Position.RW: 0.0,
-            Position.GK: 0.0,
-        }
-        return _MAP[self]
+        return _DEFENSIVENESS_MAP[self]
 
     @property
     def offensiveness(self) -> float:
-        return 2.0 - self.defensiveness  # since they sum to 2 always
+        return (
+            2.0 - self.defensiveness if self.role != Role.Keeper else 0.0
+        )  # since they sum to 2 always
 
     @classmethod
     def randomPositionFromRole(cls, role: Optional[Role]) -> "Position":
-        role = role or Role.random()
+        if role is None:
+            role = random.choices(
+                list(_ROLE_WEIGHTS),
+                weights=_ROLE_WEIGHTS.values(),
+                k=1,
+            )[0]
+
         positions = [p for p in cls if p.role == role]
         return random.choice(positions)
 
     @classmethod
     def random(cls) -> "Position":
         return cls.randomPositionFromRole(None)
+
+
+_ROLE_MAP = {
+    Position.LW: Role.Attacker,
+    Position.ST: Role.Attacker,
+    Position.RW: Role.Attacker,
+    # ----------------------------
+    Position.LM: Role.Midfielder,
+    Position.CM: Role.Midfielder,
+    Position.CAM: Role.Midfielder,
+    Position.CDM: Role.Midfielder,
+    Position.RM: Role.Midfielder,
+    # ----------------------------
+    Position.LB: Role.Defender,
+    Position.CB: Role.Defender,
+    Position.RB: Role.Defender,
+    # ----------------------------
+    Position.GK: Role.Keeper,
+}
+
+_DEFENSIVENESS_MAP = {
+    Position.LB: 2.0,
+    Position.CB: 2.0,
+    Position.RB: 2.0,
+    Position.LM: 1.0,
+    Position.CDM: 1.5,
+    Position.CM: 1.0,
+    Position.CAM: 0.5,
+    Position.RM: 1.0,
+    Position.LW: 0.0,
+    Position.ST: 0.0,
+    Position.RW: 0.0,
+    Position.GK: 0.0,
+}
