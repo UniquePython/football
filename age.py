4327deb6713d24b9432bc5e53a0acc1d4a0a9257
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-06-14 10:21:25 +0530

    add validation for max < min in age

--- age.py
+++ age.py
@@ -7,6 +7,9 @@ class AgeRange:
     max: int
 
     def __post_init__(self):
+        if self.min > self.max:
+            raise ValueError(f"min ({self.min}) cannot exceed max ({self.max})")
+
         self.min = self.min if self.min >= 16 else 16
         self.max = self.max if self.max <= 40 else 40
 
